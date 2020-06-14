b067c750f557fc1511f2dd49efd647e2add67588
karim amzil <karimamzil19@gmail.com>
2020-06-14 20:53:17 +0100

    ajout de welcome.jsx

---
+++ src/Welcome.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+export class Welcome extends Component {
+    constructor(){
+        super();
+        this.state={
+            count:0
+        }
+    }
+ addOne(){this.setState({count:this.state.count+1})}
+ removeOne() {this.setState({count:this.state.count-1})}
+ render() {
+        return (
+            <div>
+            <h1>welcome { this.props.name}</h1>  
+        <p>Mon compteur {this.state.count}</p>
+         <button onClick={()=>this.addOne()}>Ajouter 1</button>   
+          <button onClick={this.removeOne.bind(this)}>Soustraire 1</button>  
+            </div>
+        )
+    }
+}
+
+export default Welcome
